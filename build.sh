e33069754977ccd35edd3087d508c235676d3425
pskevin <25983646+pskevin@users.noreply.github.com>
2020-03-11 23:13:54 -0500

    adding multi-platform build support

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
-cd cluster
-go build *.go
+#!/usr/bin/env bash
 
-cd ../master
-go build *.go
+package_root="github.com/pskevin/shutter"
+package_names=("cluster" "master")
+platforms=("windows/amd64" "darwin/386" "linux/amd64")
+
+for package_name in "${package_names[@]}"
+do
+    for platform in "${platforms[@]}"
+    do
+        platform_split=(${platform//\// })
+        GOOS=${platform_split[0]}
+        GOARCH=${platform_split[1]}
+        output_name=$package_name'@'$GOOS'-'$GOARCH
+        if [ $GOOS = "windows" ]; then
+            output_name+='.exe'
+        fi
+        package=$package_root'/'$package_name
+        printf "\nBuilding $package as $output_name\n"
+        env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+        if [ $? -ne 0 ]; then
+            echo 'An error has occurred! Aborting the script execution...'
+            exit 1
+        fi
+    done
+done
--- requirements.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
